23abd327763d00567789c41296f5560d42d326cf
Andy Kenney <a.kenney@utoronto.ca>
2023-09-08 11:49:30 -0400

    remove st.data

--- NWAnalytics5.py
+++ NWAnalytics5.py
@@ -323,7 +323,7 @@ if fileName is not None:
     
         # select_df = aggFilter(df)
         select_df = df
-        st.data_editor(select_df)
+        select_df
 
         fig = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
                  cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
