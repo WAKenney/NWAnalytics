6d789bc19d3c853b9443dc43fd874f3d7c961b4d
Andy Kenney <a.kenney@utoronto.ca>
2023-09-08 11:56:23 -0400

    back to Aggrid

--- NWAnalytics5.py
+++ NWAnalytics5.py
@@ -321,27 +321,7 @@ if fileName is not None:
 
         # display the selected data (select_df) using AgGrid
     
-        # select_df = aggFilter(df)
-        select_df = df
-        select_df
-
-import plotly.figure_factory as ff
-
-data_matrix = [['Country', 'Year', 'Population'],
-               ['United States', 2000, 282200000],
-               ['Canada', 2000, 27790000],
-               ['United States', 2005, 295500000],
-               ['Canada', 2005, 32310000],
-               ['United States', 2010, 309000000],
-               ['Canada', 2010, 34000000]]
-
-fig = ff.create_table(data_matrix)
-st.write(fig.show())
-
-
-
-
-
+        select_df = aggFilter(df)
 
         
 def checkData(chkData):
